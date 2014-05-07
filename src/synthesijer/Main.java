9673e9787ff6037c56266017be39e17f10477f42
Takefumi MIYOSHI <miyo@wasamon.net>
2014-05-07 09:34:48 +0900

    tweak

--- src/synthesijer/Main.java
+++ src/synthesijer/Main.java
@@ -5,17 +5,21 @@ import java.io.FileOutputStream;
 import java.io.PrintWriter;
 
 public class Main {
-
+	
 	public static void main(String... args) throws FileNotFoundException{
 		openjdk.com.sun.tools.javac.main.Main compiler = new openjdk.com.sun.tools.javac.main.Main("javac", new PrintWriter(System.err, true));
 		int err = compiler.compile(args);
 		if(err == 0){
-			PrintWriter dest = new PrintWriter(new FileOutputStream("dump.xml"), true);
-			Manager.INSTANCE.dumpAsXML(dest);
-			dest.close();
+			dump("dump.xml");
 			Manager.INSTANCE.generate();
 		}
 		System.exit(err);
+	}
+	
+	public static void dump(String destName) throws FileNotFoundException{
+		PrintWriter dest = new PrintWriter(new FileOutputStream(destName), true);
+		Manager.INSTANCE.dumpAsXML(dest);
+		dest.close();
 	}
 
 }
