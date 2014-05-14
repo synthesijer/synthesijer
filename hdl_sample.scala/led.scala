7e8cce5f47234f83d4a8a2b4844b101fba1c86eb
Takefumi MIYOSHI <miyo@wasamon.net>
2014-05-14 21:27:53 +0900

    bugfix of led.scala

--- hdl_sample.scala/led.scala
+++ hdl_sample.scala/led.scala
@@ -1,65 +1,63 @@
 import synthesijer.hdl._
 import synthesijer.hdl.expr._
 
-object led extends HDLmodule{
+class led(name:String, sysClkName:String, sysRsetName:String) extends HDLModule(name, sysClkName, sysRsetName){
 
-  def gen_led(): HDLModule = {
-    val m = new HDLModule("led", "clk", "reset");
-    val q = m.newPort("q", HDLPort.DIR.OUT, HDLPrimitiveType.genBitType());
+  def this() = this("led", "clk", "reset");
 
-    // q <= counter(24)
-    val sig = q.getSrcSignal();
-    val counter = m.newSignal("counter", HDLPrimitiveType.genSignedType(32));
-    sig.setAssign(null, m.newExpr(HDLOp.REF, counter, 5));
+  val q = newPort("q", HDLPort.DIR.OUT, HDLPrimitiveType.genBitType());
+
+  // q <= counter(24)
+  val sig = q.getSignal();
+  val counter = newSignal("counter", HDLPrimitiveType.genSignedType(32));
+  sig.setAssign(null, newExpr(HDLOp.REF, counter, 5));
 		
-    // at main state, counter <= counter + 1
-    val seq = m.newSequencer("main");
-    val ss = seq.getIdleState();
-    counter.setAssign(ss, m.newExpr(HDLOp.ADD, counter, 1));
-		
-    HDLUtils.generate(m, HDLUtils.VHDL);
-    HDLUtils.generate(m, HDLUtils.Verilog);
+  // at main state, counter <= counter + 1
+  val seq = newSequencer("main");
+  val ss = seq.getIdleState();
+  counter.setAssign(ss, newExpr(HDLOp.ADD, counter, 1));
 
-    return m;
-  }
+}
 
-  def gen_sim(led: HDLModule): HDLModule = {
-    val m = new HDLSimModule("led_sim");
-    val inst = m.newModuleInstance(led, "U");
+class led_sim(led:HDLModule, name:String) extends HDLSimModule(name){
 
-    val clk = m.newSignal("clk", HDLPrimitiveType.genBitType());
-    val reset = m.newSignal("reset", HDLPrimitiveType.genBitType());
-    val counter = m.newSignal("counter", HDLPrimitiveType.genSignedType(32));
+  def this(led:HDLModule) = this(led, "led_sim");
+  val inst = newModuleInstance(led, "U");
 
-    val seq = m.newSequencer("main");
-    seq.setTransitionTime(10);
+  val clk = newSignal("clk", HDLPrimitiveType.genBitType());
+  val reset = newSignal("reset", HDLPrimitiveType.genBitType());
+  val counter = newSignal("counter", HDLPrimitiveType.genSignedType(32));
 
-    val ss = seq.getIdleState();
-    val s0 = seq.addSequencerState("S0");
-    ss.addStateTransit(s0);
-    s0.addStateTransit(ss);
+  val seq = newSequencer("main");
+  seq.setTransitionTime(10);
 
-    clk.setAssign(ss, HDLConstant.LOW);
-    clk.setAssign(s0, HDLConstant.HIGH);
+  val ss = seq.getIdleState();
+  val s0 = seq.addSequencerState("S0");
+  ss.addStateTransit(s0);
+  s0.addStateTransit(ss);
 
-    val expr = m.newExpr(HDLOp.ADD, counter, 1);
-    counter.setAssign(ss, expr);
-    counter.setAssign(s0, expr);
+  clk.setAssign(ss, HDLConstant.LOW);
+  clk.setAssign(s0, HDLConstant.HIGH);
 
-    reset.setResetValue(HDLConstant.LOW);
-    reset.setAssign(ss, m.newExpr(HDLOp.IF, m.newExpr(HDLOp.AND, m.newExpr(HDLOp.GT, counter, 3), m.newExpr(HDLOp.LT, counter, 8)), HDLConstant.HIGH, HDLConstant.LOW));
+  val expr = newExpr(HDLOp.ADD, counter, 1);
+  counter.setAssign(ss, expr);
+  counter.setAssign(s0, expr);
 
-    inst.getSignalForPort("clk").setAssign(null, clk);
-    inst.getSignalForPort("reset").setAssign(null, reset);
+  reset.setResetValue(HDLConstant.LOW);
+  reset.setAssign(ss, newExpr(HDLOp.IF, newExpr(HDLOp.AND, newExpr(HDLOp.GT, counter, 3), newExpr(HDLOp.LT, counter, 8)), HDLConstant.HIGH, HDLConstant.LOW));
 
-    HDLUtils.generate(m, HDLUtils.VHDL);
-    HDLUtils.generate(m, HDLUtils.Verilog);
+  inst.getSignalForPort("clk").setAssign(null, clk);
+  inst.getSignalForPort("reset").setAssign(null, reset);
+}
 
-    return m;
-  }
-
+object Main{
   def main(args: Array[String]) {
-    val led = gen_led();
-    gen_sim(led);
+    val m = new led();
+    HDLUtils.generate(m, HDLUtils.VHDL);
+    HDLUtils.generate(m, HDLUtils.Verilog);
+
+    val sim = new led_sim(m);
+    HDLUtils.generate(sim, HDLUtils.VHDL);
+    HDLUtils.generate(sim, HDLUtils.Verilog);
   }
 }
