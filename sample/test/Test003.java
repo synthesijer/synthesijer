f02a05a76e6f0416e6203dd751f1567194095342
Takefumi MIYOSHI <miyo@wasamon.net>
2014-07-03 21:59:33 +0900

    added a sample, which creates a new instance and calles the methods

---
+++ sample/test/Test003.java
@@ -0,0 +1,13 @@
+
+public class Test003 {
+	
+	private final Test002 t = new Test002();
+	
+	public void test(){
+		t.init();
+		t.set(0, 100);
+		t.set(1, 50);
+		t.set(3, t.get(0) + t.get(1));
+	}
+
+}
