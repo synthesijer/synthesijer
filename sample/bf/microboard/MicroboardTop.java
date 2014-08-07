32fe4ca56e80e05767ef561e2118c9e87e428802
Takefumi MIYOSHI <miyo@wasamon.net>
2014-08-07 15:24:29 +0900

    add compile project for Microboard with Xilinx ISE

---
+++ sample/bf/microboard/MicroboardTop.java
@@ -0,0 +1,31 @@
+
+import java.io.IOException;
+
+import synthesijer.hdl.*;
+import synthesijer.hdl.expr.*;
+
+public class MicroboardTop{
+	
+	public static void main(String... args) throws IOException{
+		HDLModule top = new HDLModule("microboard_top", "CLOCK_Y3", "USER_RESET");
+		HDLPort din = top.newPort("USB_RS232_RXD", HDLPort.DIR.IN, HDLPrimitiveType.genBitType());
+		HDLPort dout = top.newPort("USB_RS232_TXD", HDLPort.DIR.OUT, HDLPrimitiveType.genBitType());
+
+		HDLModule m = new HDLModule("HW", "clk", "reset");
+		HDLPort run_req  = m.newPort("main_req", HDLPort.DIR.IN, HDLPrimitiveType.genBitType());
+		HDLPort run_busy = m.newPort("main_busy", HDLPort.DIR.OUT, HDLPrimitiveType.genBitType());
+		HDLPort tx_dout  = m.newPort("b_io_obj_tx_dout", HDLPort.DIR.OUT, HDLPrimitiveType.genBitType());
+		HDLPort rx_din   = m.newPort("b_io_obj_rx_din", HDLPort.DIR.IN, HDLPrimitiveType.genBitType());
+		HDLInstance inst = top.newModuleInstance(m, "U");
+		
+		inst.getSignalForPort("clk").setAssign(null, top.getSysClk().getSignal());
+		inst.getSignalForPort("reset").setAssign(null, top.getSysReset().getSignal());
+		inst.getSignalForPort(run_req.getName()).setAssign(null, HDLPreDefinedConstant.HIGH); // always high to start immediately
+		dout.getSignal().setAssign(null, inst.getSignalForPort(tx_dout.getName()));
+		inst.getSignalForPort(rx_din.getName()).setAssign(null, din.getSignal());
+		
+		HDLUtils.generate(top, HDLUtils.VHDL);
+		HDLUtils.generate(top, HDLUtils.Verilog);
+	}
+}
+
