bd50f03059c5041d7a91ad284ef41f537337ad49
Takefumi MIYOSHI <miyo@wasamon.net>
2015-04-25 17:47:18 +0900

    commit a forgot file

---
+++ sample/test/Test013.java
@@ -0,0 +1,9 @@
+public class Test013{
+	private final Test013_A obj0 = new Test013_B();
+	private final Test013_A obj1 = new Test013_C();
+	
+	public void test(){
+		int a = obj0.hoge();
+		int b = obj1.hoge();
+	}
+}
