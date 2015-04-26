5f02bc39504ad5a2e6f40f15100cbff4eb09e484
Takefumi MIYOSHI <miyo@wasamon.net>
2015-04-26 18:47:22 +0900

    add cleanup.sh to remove temprary files for CoreGen

---
+++ lib/Xilinx/Spartan6/cleanup.sh
@@ -0,0 +1,12 @@
+rm -f *.tcl
+rm -f *.v*
+rm -f *.asy *.gise *.ngc *.sym *.txt
+rm -f coregen.cgc
+rm -f coregen.cgp
+rm -f coregen.log
+rm -rf demo_tb
+rm -rf f*_ip
+rm -rf div*_ip
+rm -rf tmp
+rm -rf xlnx_auto_0_xdb
+rm -rf _xmsgs
