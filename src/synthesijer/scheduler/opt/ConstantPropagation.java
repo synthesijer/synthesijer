35e1550b0b3578f4e8225cebd026ca27cbfc3e54
Takefumi MIYOSHI <miyo@wasamon.net>
2015-10-05 09:12:54 +0900

    started to implement constant propagation

---
+++ src/synthesijer/scheduler/opt/ConstantPropagation.java
@@ -0,0 +1,35 @@
+package synthesijer.scheduler.opt;
+
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.Hashtable;
+
+import synthesijer.scheduler.Op;
+import synthesijer.scheduler.Operand;
+import synthesijer.scheduler.SchedulerBoard;
+import synthesijer.scheduler.SchedulerInfo;
+import synthesijer.scheduler.SchedulerItem;
+import synthesijer.scheduler.SchedulerSlot;
+import synthesijer.scheduler.VariableOperand;
+
+public class ConstantPropagation implements SchedulerInfoOptimizer{
+	
+    public static final boolean DEBUG = false;
+
+    public SchedulerInfo opt(SchedulerInfo info){
+	SchedulerInfo result = info.getSameInfo();
+	for(SchedulerBoard b: info.getBoardsList()){
+	    result.addBoard(b);
+	}
+	return result;
+    }
+
+    public String getKey(){
+	return "null_optimizer";
+    }
+    
+    private void printVars(){
+	
+    }
+	
+}
