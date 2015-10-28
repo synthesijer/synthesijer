d0efd04a6929452b9d1e0c156c6eaedca24bcfba
Takefumi MIYOSHI <miyo@wasamon.net>
2015-10-28 17:05:47 +0900

    typo synthesijer_extra_libs -> synthesijer_extra-libs in sample/bd/TB-7Z-020/Makefile

--- sample/bd/TB-7Z-020/SinTableRom.java
+++ sample/bd/TB-7Z-020/SinTableRom.java
@@ -8,7 +8,6 @@ import synthesijer.hdl.HDLSignal;
 import synthesijer.hdl.HDLUtils;
 import synthesijer.hdl.expr.HDLPreDefinedConstant;
 import synthesijer.hdl.sequencer.SequencerState;
-import synthesijer.utils.Utils;
 
 public class SinTableRom extends HDLModule{
 
@@ -16,12 +15,12 @@ public class SinTableRom extends HDLModule{
     
 	public SinTableRom(String... args){
 		super("sintable_rom", "clk", "reset");
-		Utils.genInputPort(this, "sintable_address", 32);
-		Utils.genInputPort(this, "sintable_din", 32);
-		Utils.genOutputPort(this, "sintable_dout", 32);
-		Utils.genOutputPort(this, "sintable_length", 32);
-		Utils.genInputPort(this, "sintable_we");
-		Utils.genInputPort(this, "sintable_oe");
+		HDLUtils.genInputPort(this, "sintable_address", 32);
+		HDLUtils.genInputPort(this, "sintable_din", 32);
+		HDLUtils.genOutputPort(this, "sintable_dout", 32);
+		HDLUtils.genOutputPort(this, "sintable_length", 32);
+		HDLUtils.genInputPort(this, "sintable_we");
+		HDLUtils.genInputPort(this, "sintable_oe");
 	}
 	
 }
