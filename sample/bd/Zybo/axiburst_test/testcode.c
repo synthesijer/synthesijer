ee691e171bf5fc284d9d69d968b5683be73844e6
Takefumi MIYOSHI <miyo@wasamon.net>
2015-11-10 10:52:54 +0900

    added a C source code to test

--- sample/bd/Zybo/axiburst_test/testcode.c
+++ sample/bd/Zybo/axiburst_test/testcode.c
@@ -5,17 +5,17 @@ int main() {
   volatile unsigned int src_data[256], dst_data[256];
   for(i = 0; i < 256; i++) src_data[i] = i;
   unsigned int *baseaddr = (unsigned int*)0x43c00000;
-  xil_printf("rn");
+  xil_printf("\r\n");
   baseaddr[1] = (unsigned int)src_data;
   baseaddr[2] = (unsigned int)dst_data;
   baseaddr[0] = 0xFFFFFFFF;
-  xil_printf("memcpy start, src=%08x dest=%08xnr", src_data, dst_data);
+  xil_printf("memcpy start, src=%08x dest=%08x\n\r", src_data, dst_data);
   while(baseaddr[0] != 0) ;
-  xil_printf("memcpy donenr");
+  xil_printf("memcpy done\n\r");
   for(i = 0; i < 256; i++){
     xil_printf("src_data[%d] = %d, ", i, src_data[i]);
-    xil_printf("dst_data[%d] = %dnr", i, dst_data[i]);
+    xil_printf("dst_data[%d] = %d\n\r", i, dst_data[i]);
     if(src_data[i] != dst_data[i]) mismatch = 1;
   }
-  (mismatch==0)? xil_printf("memcpy success!nr") : xil_printf("memcpy failnr"); return 0;
+  (mismatch==0)? xil_printf("memcpy success!\n\r") : xil_printf("memcpy fail\n\r"); return 0;
 }
