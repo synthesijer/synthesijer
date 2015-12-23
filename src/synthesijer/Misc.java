70f62ef7165aebe680da123fbe11dd55186fa8b6
Takefumi MIYOSHI <miyo@wasamon.net>
2015-12-23 11:14:38 +0900

    add Misc.java

---
+++ src/synthesijer/Misc.java
@@ -0,0 +1,36 @@
+package synthesijer;
+
+import synthesijer.hdl.*;
+
+public class Misc{
+
+    private static final String NL = System.getProperty("line.separator");
+
+    public static String genUCF(HDLModule m){
+	String s = "";
+	for(HDLPort p: m.getPorts()){
+	    s += "NET " + p.getName() + " LOC = " + p.getPinID();
+	    String a = p.getIoAttr();
+	    if(a != null){
+		s += " | IOSTANDARD = " + a;
+	    }
+	    s += ";" + NL;
+	}
+	return s;
+    }
+
+    public static String genXDC(HDLModule m){
+	String s = "";
+	for(HDLPort p: m.getPorts()){
+	    s += "set_property PACKAGE_PIN " + p.getPinID()  + " [get_ports " + p.getName() + "]" + NL;
+	    String a = p.getIoAttr();
+	    if(a != null){
+		s += String.format("set_property IOSTANDARD %s [get_ports %s]", p.getIoAttr(), p.getName());
+	    }
+	    s += NL;
+	}
+	return s;
+    }
+
+    
+}
