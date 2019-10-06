8e3b4dd63bc4db171be2a30e109f980bb5b8e4dc
Takefumi MIYOSHI <miyo@wasamon.net>
2019-10-06 21:56:34 +0900

    sample/bd has been move to extra-libs, because they depetnds on extra-libs

--- sample/bd/TB-7Z-020/compile.sh
+++
@@ -1,7 +0,0 @@
-java -cp ../../../extra-libs/bin:../../../target/synthesijer:. \
-  synthesijer.Main \
-  ../../../extra-libs/src/synthesijer/lib/axi/SimpleAXIMemIface32RTL.java \
-  ../../../extra-libs/src/synthesijer/lib/axi/SimpleAXIMemIface32RTLTest.java \
-  TestFrame.java \
-  RGBTest.java \
-  SinTableRom.java
