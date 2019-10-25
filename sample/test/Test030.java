848c33c7843a8a8d7b7da65f21b7f7acd99194fa
Takefumi MIYOSHI <miyo@wasamon.net>
2019-10-25 13:41:52 +0900

    add an example using @Pipeline annotation

---
+++ sample/test/Test030.java
@@ -0,0 +1,14 @@
+
+import synthesijer.rt.*;
+
+public class Test030{
+
+	public void test(int[] a, int[] b, int[] c){
+		for(@Pipeline int i = 0; i < 1024; i ++){
+			c[i] = a[i] + b[i];
+		}
+	}
+
+
+	
+}
